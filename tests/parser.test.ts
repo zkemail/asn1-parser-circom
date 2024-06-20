2af29f88374d4c69091868e8cee2ed43b9dbcbcd
0xvikasrushi <0xvikas@gmail.com>
2024-06-21 02:57:57 +0530

    test: added utf8

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -32,6 +32,41 @@ describe("DecodeLength", () => {
   });
 });
 
+describe("UTF8StringParser", () => {
+  let circuit: WitnessTester<["in"], ["out"]>;
+  const input = [0x0c, 0x09, 0x48, 0x79, 0x64, 0x65, 0x72, 0x61, 0x62, 0x61, 0x64]; // hyderabad
+  const N = input.length;
+
+  before(async () => {
+    circuit = await circomkit.WitnessTester(`UTF8StringParser_${N}`, {
+      file: "parser",
+      template: "UTF8StringParser",
+      params: [N],
+    });
+  });
+
+  it("It Should take parse UTF8String", async () => {
+    const expectedOutput = Array.from(Buffer.from("Hyderabad"));
+    await circuit.calculateWitness({ in: input });
+    await circuit.expectPass({ in: input }, { out: expectedOutput });
+  });
+
+  it("It Should take parser UTF8String", async () => {
+    const input = [
+      0x0c, 0x11, 0x4f, 0x72, 0x67, 0x61, 0x6e, 0x69, 0x7a, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x44, 0x75, 0x6d, 0x6d, 0x79,
+    ];
+    const expectedOutput = Array.from(Buffer.from("OrganizationDummy"));
+
+    const N = input.length;
+    circuit = await circomkit.WitnessTester(`UTF8StringParser_${N}`, {
+      file: "parser",
+      template: "UTF8StringParser",
+      params: [N],
+    });
+    await circuit.expectPass({ in: input }, { out: expectedOutput });
+  });
+});
+
 describe("Parser", () => {
   let circuit: WitnessTester<["in"], ["out"]>;
   const N = SAMPLE_DER.length;
