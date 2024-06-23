ea40ac74e231a3c04150717f4d575e9180c0df74
0xvikasrushi <0xvikas@gmail.com>
2024-06-23 22:44:27 +0530

    feat: generic parser in ts

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-import { SAMPLE_BER } from "./constant";
+import { SAMPLE_BER, SAMPLE_BER_EXPECTED_0ID } from "./constant";
 import { parseOid, parseUTFString } from "./parser-utils";
 
 function calculateOffSet(buff: number) {
@@ -16,8 +16,15 @@ function Parser(data: number[]) {
   while (i < data.length - 1) {
     const firstByte = data[i];
     const secondByte = data[i + 1];
-    if (firstByte === 0x30 || firstByte === 0xa0) {
-      const isLongForm = (secondByte & 0x80) !== 0;
+    if (
+      firstByte === 0x30 ||
+      firstByte === 0xa0 ||
+      firstByte == 0x31 ||
+      firstByte == 0xa3 ||
+      firstByte == 0xa1 ||
+      firstByte == 0xa4
+    ) {
+      const isLongForm = (secondByte & 0x80) === 0 ? false : true;
       if (isLongForm) {
         const offset = calculateOffSet(secondByte);
         const endIndex = i + offset + 2;
@@ -27,9 +34,29 @@ function Parser(data: number[]) {
         ASN_ARRAY.push(data.slice(i, i + 2));
         i += 2;
       }
-    } else if (firstByte == 0x31) {
-      ASN_ARRAY.push(data.slice(i, i + 2));
-      i += 2;
+      // octet string
+    } else if (firstByte == 0x04) {
+      const isLongForm = (secondByte & 0x80) === 0 ? false : true;
+      let length = 0;
+      if (isLongForm) {
+        let numBytes = secondByte & 0x7f;
+        let temp = numBytes;
+        let currentIndex = i + 2;
+        while (numBytes > 0) {
+          length = (length << 8) | data[currentIndex];
+          numBytes--;
+          ++currentIndex;
+        }
+        const startIndex = i;
+        const endIndex = startIndex + length + 2 + temp;
+        ASN_ARRAY.push(data.slice(i, endIndex));
+        i = endIndex;
+      } else {
+        const startIndex = i;
+        const endIndex = startIndex + secondByte + 2;
+        ASN_ARRAY.push(data.slice(i, endIndex));
+        i = endIndex;
+      }
     } else {
       const startIndex = i;
       const endIndex = startIndex + secondByte + 2;
@@ -42,15 +69,18 @@ function Parser(data: number[]) {
 
 const sorted = Parser(SAMPLE_BER);
 
+const oidArray: string[] = [];
 sorted.forEach((e) => {
   if (e[0] === 0x06) {
-    console.log(parseOid(e));
+    oidArray.push(parseOid(e));
   }
-
   if (e[0] == 0x0c) {
     console.log(parseUTFString(e));
   }
   if (e[0] == 0x04) {
+    // TODO OCTECT PARSING
     console.log(parseUTFString(e));
   }
 });
+console.log(oidArray.length);
+console.log(oidArray.length == SAMPLE_BER_EXPECTED_0ID.length);
