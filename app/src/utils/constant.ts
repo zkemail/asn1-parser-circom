e016d87b6ecbee83a20387caa14fd5f37112ec6c
0xvikasrushi <0xvikas@gmail.com>
2024-07-14 14:59:02 +0530

    bug: zkey link fixed

--- app/src/utils/constant.ts
+++ app/src/utils/constant.ts
@@ -4,9 +4,9 @@ import Vkey from "../assets/groth16_vkey.json";
 // ? need to change once repo is open source
 
 export const wasm_path =
-  "https://raw.githubusercontent.com/zk-email/asn1-parser-circom/main/app/src/assets/utf8prover.wasm";
+  "https://raw.githubusercontent.com/zkemail/asn1-parser-circom/main/app/src/assets/utf8prover.wasm";
 export const zkey_path =
-  "https://raw.githubusercontent.com/zk-email/asn1-parser-circom/main/app/src/assets/utf8prover.zkey";
+  "https://raw.githubusercontent.com/zkemail/asn1-parser-circom/main/app/src/assets/utf8prover.zkey";
 export const vkey_path = Vkey;
 
 export const OPTIONS = ["DER", "BER", "X.509"];
