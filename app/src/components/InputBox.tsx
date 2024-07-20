86ac4e6e5de71ddb98b31a7556e2900be87098a6
0xvikasrushi <0xvikas@gmail.com>
2024-07-20 12:09:58 +0530

    feat: pdf support

--- app/src/components/InputBox.tsx
+++ app/src/components/InputBox.tsx
@@ -1,5 +1,7 @@
+import { Buffer } from "buffer";
 import { ChangeEvent, useState } from "react";
 import { OPTIONS } from "../utils/constant";
+import { DER_SAMPLE_CONTENT, SAMPLE_BER_CONTENT, SAMPLE_X_509_CONTENT } from "../utils/samples";
 
 interface InputBoxProps {
   certificateContent: string;
@@ -11,16 +13,23 @@ export default function InputBox({ certificateContent, onCertificateContentChang
   const [certificateType, setCertificateType] = useState("DER");
   const [isSampleCheck, setIsSampleCheck] = useState(false);
 
-  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e: ProgressEvent<FileReader>) => {
-        const content = e.target?.result as string;
-        onCertificateContentChange(content, "file");
-      };
-      reader.readAsText(file);
+  const handleFileUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]!;
+
+    const arrayBuffer = Buffer.from(await file.arrayBuffer());
+    const pdfContent = arrayBuffer.toString("utf8");
+
+    const contentsRegex = /\/Contents\s*<([0-9A-Fa-f]+)>/;
+    const match = contentsRegex.exec(pdfContent);
+
+    if (!match) {
+      console.error("No match found");
+      return;
     }
+    const hexEContnent = match[1];
+    const signature = Buffer.from(hexEContnent, "hex").toString("base64");
+
+    onCertificateContentChange(signature, "base64");
   };
 
   const handleTextareaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -29,18 +38,19 @@ export default function InputBox({ certificateContent, onCertificateContentChang
   };
 
   const fetchCertificateContent = (type: string) => {
-    const fileUrl = {
-      DER: "https://raw.githubusercontent.com/zkemail/asn1-parser-circom/main/samples/sig-p256-der.txt",
-      BER: "https://raw.githubusercontent.com/zkemail/asn1-parser-circom/main/samples/sig-p256-ber.txt",
-      "X.509": "https://raw.githubusercontent.com/zkemail/asn1-parser-circom/main/samples/X.509.txt",
-    }[type];
-
-    fetch(`${fileUrl}`)
-      .then((response) => response.text())
-      .then((text) => {
-        onCertificateContentChange(text, type);
-      })
-      .catch((error) => console.error("Error fetching text file:", error));
+    switch (type) {
+      case "DER":
+        onCertificateContentChange(DER_SAMPLE_CONTENT, "DER");
+        break;
+      case "BER":
+        onCertificateContentChange(SAMPLE_BER_CONTENT, "BER");
+        break;
+      case "X.509":
+        onCertificateContentChange(SAMPLE_X_509_CONTENT, "X.509");
+        break;
+      default:
+        break;
+    }
   };
 
   const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -54,54 +64,74 @@ export default function InputBox({ certificateContent, onCertificateContentChang
     if (event.target.checked) {
       fetchCertificateContent(certificateType);
     } else {
-      onCertificateContentChange("", ""); // Clear content in parent component
+      onCertificateContentChange("", "");
     }
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-4">ASN.1 Parser</h1>
-      <div className="flex flex-col items-center p-6 rounded shadow-md">
-        <input
-          type="file"
-          accept="application/pdf"
-          className="mb-4 p-2 border rounded w-96 h-12"
-          onChange={handleFileUpload}
-        />
-        <textarea
-          className="w-full p-4 border rounded mb-4 h-96"
-          placeholder="Paste certificate file here"
-          value={certificateContent}
-          onChange={handleTextareaChange}
-        ></textarea>
-        <div className="mb-4 w-full">
-          <label className="flex items-center mb-2">
-            <input type="checkbox" checked={isSampleCheck} onChange={handleCheckboxChange} className="mr-2" />
-            <span>Use sample certificate</span>
-          </label>
-          {isSampleCheck && (
-            <>
-              {OPTIONS.map((option) => (
-                <div className="flex items-center mb-2" key={option}>
-                  <input
-                    type="radio"
-                    id={option}
-                    name="certificateType"
-                    value={option}
-                    checked={certificateType === option}
-                    onChange={handleRadioChange}
-                    className="mr-2"
-                  />
-                  <label htmlFor={option}>{option}</label>
-                </div>
-              ))}
-            </>
-          )}
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold mb-4">ZK ASN.1 Parser</h1>
+        <div className="flex flex-col items-center p-6 rounded shadow-md">
+          <input
+            type="file"
+            accept="application/pdf"
+            className={`mb-4 p-2 border rounded w-96 h-12 ${isSampleCheck ? "opacity-50 cursor-not-allowed" : ""}`}
+            onChange={handleFileUpload}
+            disabled={isSampleCheck}
+          />
+
+          <div className="mb-4 w-full">
+            <label className="flex items-center mb-2">
+              <input type="checkbox" checked={isSampleCheck} onChange={handleCheckboxChange} className="mr-2" />
+              <span>Use sample certificate</span>
+            </label>
+            {isSampleCheck && (
+              <>
+                {OPTIONS.map((option) => (
+                  <div className="flex items-center mb-2" key={option}>
+                    <input
+                      type="radio"
+                      id={option}
+                      name="certificateType"
+                      value={option}
+                      checked={certificateType === option}
+                      onChange={handleRadioChange}
+                      className="mr-2"
+                    />
+                    <label htmlFor={option}>{option}</label>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+
+          <textarea
+            className="w-full p-4 border rounded mb-4 h-96"
+            placeholder="Paste certificate file here"
+            value={certificateContent}
+            onChange={handleTextareaChange}
+          ></textarea>
+
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onClick={onGenerateProof}>
+            Check Parameters to Generate Proof
+          </button>
         </div>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onClick={onGenerateProof}>
-          Check Parameters to Generate Proof
-        </button>
+
+        <div className="flex items-center bg-gradient-to-r rounded-lg shadow-lg px-6 py-4 mb-8">
+          <p className="text-white font-medium mr-4 flex items-center">
+            Project is supported by &nbsp;
+            <a
+              href="https://github.com/zkemail"
+              className="block text-center underline transition duration-300 hover:text-blue-600"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ZkEmail
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
